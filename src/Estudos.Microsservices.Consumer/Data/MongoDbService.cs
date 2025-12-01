96a1a5f47160230fd4e71a95bd51315b77217fda
Leonardo Picanço <leonardocarlosreis@gmail.com>
2025-11-30 23:29:44 -0300

    Correção para acessar o mongo via docker

--- src/Estudos.Microsservices.Consumer/Data/MongoDbService.cs
+++ src/Estudos.Microsservices.Consumer/Data/MongoDbService.cs
@@ -1,6 +1,4 @@
-﻿using Estudos.Microsservices.Consumer.Entities;
-using Estudos.Microsservices.Contratos;
-using MongoDB.Driver;
+﻿using MongoDB.Driver;
 using MongoDB.Driver.Core.Extensions.DiagnosticSources;
 
 namespace Estudos.Microsservices.Consumer.Data;
@@ -16,18 +14,14 @@ public class MongoDbService
         try
         {
             _configuration = configuration;
-
             var connectionString = _configuration.GetConnectionString("DbConnection");
             var mongoUrl = MongoUrl.Create(connectionString);
             var clientSettings = MongoClientSettings.FromUrl(mongoUrl);
 
-            // Adicione esta linha crucial para habilitar a emissão de eventos de diagnóstico
             clientSettings.ClusterConfigurator = cb => cb.Subscribe(new DiagnosticsActivityEventSubscriber());
 
             var mongoClient = new MongoClient(clientSettings);
             _database = mongoClient.GetDatabase(mongoUrl.DatabaseName);
-            //var a = _database.GetCollection<BusMessageDb>("testecollection");
-            //a.InsertOne(new BusMessageDb() { Message = "1234123" });
         }
         catch(Exception ex)
         {
